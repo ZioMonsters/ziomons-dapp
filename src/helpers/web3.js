ff6245d969cae8efb57d09ac702d7920338e204f
Nicolò Fuccella <nico.fuccella@gmail.com>
2018-09-08 09:04:21 +0200

    web3 init

--- src/helpers/web3.js
+++ src/helpers/web3.js
@@ -1,8 +1,31 @@
 import Web3 from "web3"
-import { abi } from "../contracts/CryptoMon.json"
+import { abi, networks } from "../contracts/CryptoMon.json"
 
-const web3 = new Web3("http://localhost:9545/")
+const web3 = new Web3("http://localhost:9545/", {
 
-export const getContractInstance = () => {
-  return new web3.eth.Contract(abi, "0x2c2b9c9a4a25e24b174f26114e8926a9f2128fe4") 
+})
+
+const contract = new web3.eth.Contract(abi, networks["4447"].address)
+
+// export const getContractInstance = () => {
+//   if (contract) {
+//     return contract
+//   } else {
+//     new web3.eth.Contract(abi, networks["4447"].address)
+//       .then(instance => {
+//         contract = instance
+//         return instance
+//       })
+//   }
+// }
+
+export const fight = ({ team, minBet, bet }) => {
+  // return getContractInstance()
+  //   .then(contract => {
+  return web3.eth.getAccounts()
+    .then(([ address ]) => {
+      return contract.methods.fight(team, minBet).send({ value: bet, from: address })
+    })
+
+    // })
 }
