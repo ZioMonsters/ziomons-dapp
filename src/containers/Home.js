4c6364350e356a13ca207d5951f9073673f91418
Nicolò Fuccella <nico.fuccella@gmail.com>
2018-09-08 19:02:39 +0200

    home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,40 +2,55 @@ import React, { Component } from "react"
 import { Button, Collapse, Well, Grid, Row, Image, Col } from "react-bootstrap"
 import { drizzleConnect } from "drizzle-react"
 import sprite from "../assets/sprite.svg"
+import ReactSVG from 'react-svg'
+import { LinkContainer } from "react-router-bootstrap"
 
 class Home extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      open: false
+      aboutUsOpen: false
     }
   }
 
   render() {
     return (
-      <Grid className = { "home" }>
+      <Grid fluid = {false} className = { "home" }>
         <Row>
-          <Image src = {sprite} />
-          <Col>
-            <h1><strong>Welcome</strong></h1>
+          <h1>Welcome</h1>
+        </Row>
+        <Row>
+          <Col md = { 4 }><ReactSVG src = { sprite } /></Col>
+          <Col md = { 8 }>
+            <blockquote>
+              <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
+              <footer><cite>Cryptomon team</cite></footer>
+            </blockquote>
           </Col>
         </Row>
         <Row className = {"info"} >
           <Row>
-            <Button onClick={ () => this.setState({ open: !this.state.open }) }>
-              What about us
-            </Button>
+            <Col md = { 6 }>
+              <LinkContainer to = {"/unbox"}>
+                <Button>
+                  Unbox your first monsters
+                </Button>
+              </LinkContainer>
+            </Col>
+            <Col md = { 6 }>
+              <Button onClick={ () => this.setState(({ aboutUsOpen}) => ({ aboutUsOpen: !aboutUsOpen })) }>
+                About us
+              </Button>
+            </Col>
           </Row>
-          <Row>
-            <Collapse in = { this.state.open }>
-              <div>
-                <Well>
-                  Anim pariatur cliche reprehenderit, enim eiusmod high life
-                  accusamus terry richardson ad squid. Nihil anim keffiyeh
-                  helvetica, craft beer labore wes anderson cred nesciunt sapiente
-                  ea proident.
-                </Well>
-              </div>
+          <Row className = { "about-us" }>
+            <Collapse in = { this.state.aboutUsOpen }>
+              <p>
+                Anim pariatur cliche reprehenderit, enim eiusmod high life
+                accusamus terry richardson ad squid. Nihil anim keffiyeh
+                helvetica, craft beer labore wes anderson cred nesciunt sapiente
+                ea proident.
+              </p>
             </Collapse>
           </Row>
         </Row>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from "react"
 import ReactDOM from "react-dom"
 import Root from "./containers/Root.js"
 import "./styles/base.css"
-import registerServiceWorker from "./registerServiceWorker"
 
 ReactDOM.render(<Root />, document.getElementById("root"))
-registerServiceWorker()
